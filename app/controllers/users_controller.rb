aac5cd02bb61f463db10e94d4698e369fbe5c198
mahboob omary <mahboob.omary@gmail.com>
2023-03-27 10:44:22 +0200

    User Model have index, view page, and update page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
   def index
     @users = User.all
   end
-  
+
+  def show
+
+  end
+
+
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "user was succesfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :full_name, :nickname, :phone, :address, :multimedia, :description, :looking_for_band, :skills, :style, :experience, profile_picture: [] )
+  end
+
 end
