c05bedeb650a15d58d70175766bd0abe03e76a8f
jane eme <janeeme23@gmail.com>
2023-03-31 11:47:39 +0200

    search done

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,4 +2,10 @@ class Band < ApplicationRecord
   belongs_to :user
   has_many :band_posts
   has_many :band_members
+  validates :band_name, presence: true
+  validates :style, presence: true
+  include PgSearch::Model
+  pg_search_scope :search_by_band_name_and_style,
+  against: [ :band_name, :style ],
+  using: { tsearch: { prefix: true } }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :bands
   end
+  resources :bands, only: :index
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
