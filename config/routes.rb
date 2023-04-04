915298ffe3d0587b0c3faaa1decb2889d3b692d0
mahboob omary <mahboob.omary@gmail.com>
2023-04-04 09:29:05 +0200

    chating is only working as a one central chating system.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :users do
-    resources :chatrooms, only: [:show, :index] do
-      resources :messages, only: :create
-    end
+    # resources :chatrooms, only: [:show, :index] do
+    #   resources :messages, only: :create
+    # end
     resources :bands do
       resources :band_members
     end
   end
 
-
+  resources :chatrooms, only: [:show, :index] do
+    resources :messages, only: :create
+  end
 end
