3b0f226aa105f2900b47b5bdbd5cf97a549c8249
mahboob omary <mahboob.omary@gmail.com>
2023-04-06 01:23:36 +0200

    Bands on the home paged are clickable now. 2. messages are now connected with the navebar. 3. on the show page of the messages you can see list of people sent you messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,6 +5,8 @@ class ChatroomsController < ApplicationController
   end
 
   def show
+    @user_chatrooms = Chatroom.where(sender: current_user).or(Chatroom.where(receiver: current_user))
+
     @chatroom = Chatroom.find(params[:id])
     @receiver = User.find(params[:id])
     @chatroom_name = @chatroom.sender == current_user ? @chatroom.receiver.full_name : @chatroom.sender.full_name
