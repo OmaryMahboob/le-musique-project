f719d4e41be80c95f9223eaf264c94468af804dc
mahboob omary <mahboob.omary@gmail.com>
2023-04-06 11:23:18 +0200

    working on the cloudinary

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,3 @@
-require 'pry-byebug'
 class Chatroom < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: :sender_id
   belongs_to :receiver, class_name: "User", foreign_key: :receiver_id
