f251a3fd7d83e3eb3d462a69ae1d31fbb6302947
mahboob omary <mahboob.omary@gmail.com>
2023-04-10 11:06:45 +0200

    skill dataset is created and seeding is also added

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,2 +1,6 @@
 class Style < ApplicationRecord
+  has_many :user_band_styles
+  has_many :bands, through: :user_band_styles, optional: true
+  has_many :users, through: :user_band_styles, optional: true
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/user_band_style.rb
+++ app/models/user_band_style.rb
@@ -1,5 +1,12 @@
 class UserBandStyle < ApplicationRecord
-  belongs_to :user
-  belongs_to :style
+  belongs_to :user, optional: true
+  belongs_to :style, optional: true
   belongs_to :band
+
+  private
+  def user_or_band_present
+    unless user.present? || band.present?
+      errors.add(:base, "User or Band must be present")
+    end
+  end
 end
