8f1a3094e4930b9f1213d55fe6d22a20442c326f
mahboob omary <mahboob.omary@gmail.com>
2023-04-11 12:05:16 +0200

    updated the UserBandsStyle validations

--- app/models/user_band_style.rb
+++ app/models/user_band_style.rb
@@ -2,5 +2,5 @@ class UserBandStyle < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :band, optional: true
   belongs_to :style
-  # validates :style_id, uniqueness: { scope: :user_id }
+  validates :style_id, uniqueness: { scope: :band_id }
 end
