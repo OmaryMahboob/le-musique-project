1730d29beb878309bdce0d7bfef80fa4e531ac85
mahboob omary <mahboob.omary@gmail.com>
2023-04-13 03:19:27 +0200

    fixed conflicts

--- app/models/band.rb
+++ app/models/band.rb
@@ -5,9 +5,11 @@ class Band < ApplicationRecord
   has_many :users, through: :band_members
   has_many :user_band_styles, dependent: :destroy
   has_many :styles, through: :user_band_styles
+  has_many_attached :multimedia
+  validates :band_name, uniqueness: true, presence: true
+  
   include PgSearch::Model
   pg_search_scope :search_by_band_name_and_style,
   against: [ :band_name, :style ],
   using: { tsearch: { prefix: true } }
-  has_many_attached :multimedia
-  validates :band_name, uniqueness: true, presence: true
+end
