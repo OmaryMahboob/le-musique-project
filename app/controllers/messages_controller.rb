c7ab0d2ee7a73a83f08202a4d07e3437fc99e40e
mahboob omary <mahboob.omary@gmail.com>
2023-04-19 23:21:57 +0200

    working on the chatroom web socket creation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,16 @@
 class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
+    @user_chatrooms = Chatroom.where(sender: current_user).or(Chatroom.where(receiver: current_user))
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      redirect_to chatroom_path(@chatroom)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string("chatrooms/show", locals: {message: @message})
+      )
+      head :ok
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
